d873e20604d3016703572f8d7c5184e574b15b3d
CherifaHamroun <hc_hamroun@esi.dz>
2020-09-11 00:46:19 +0200

    Passing The Welcoming Screen

--- app/src/main/java/com/example/quizzapp/MainActivity.kt
+++ app/src/main/java/com/example/quizzapp/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.example.quizzapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN // Removing the top bar
+        btn_Start.setOnClickListener {
+            if (et_name.text.toString().isEmpty()){
+                Toast.makeText(this,"Enter your name please",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                val intent = Intent(this,QuizzQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
